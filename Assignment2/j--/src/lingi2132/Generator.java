bef3d57d8dd5a7ea5423cb23f502f3f83dc25c0a
louisna <navarre.louis@student.uclouvain.be>
2020-03-09 17:13:43 +0100

    la moula

--- Assignment2/j--/src/lingi2132/Generator.java
+++ Assignment2/j--/src/lingi2132/Generator.java
@@ -29,14 +29,18 @@ public class Generator extends GlobalGenerator {
 	
 	public void generateClass() {
 		CLEmitter output = new CLEmitter(true);
-		output.destinationDir(outputDir);
+		output.destinationDir(super.outputDir);
 		
 		ArrayList<String> mods = new ArrayList<>();
 		mods.add("public");
-		// mods.add("super"); ???? ADD ????
+		mods.add("super");
+
 		
 		/* Class */
 		output.addClass(mods, "packageOfClassToGenerate/ClassToGenerate", "java/lang/Object", null, false); // null for last argument ?
+		
+		mods.clear();
+		mods.add("public");
 		
 		/* Constructor */
 		output.addMethod(mods, "<init>", "()V", null, false);
@@ -46,34 +50,30 @@ public class Generator extends GlobalGenerator {
 		
 		/* Method gcd */
 		mods.add("static");
-		output.addMethod(mods, "gcd", "(I,I)I", null, false);
-		output.addBranchInstruction(GOTO, "out");
-		output.addLabel("statement");
-		// Make if - else statement
+		output.addMethod(mods, "gcd", "(II)I", null, false);
+		output.addLabel("in");
+		output.addNoArgInstruction(ILOAD_1);
+		output.addBranchInstruction(IFEQ, "out");
 		output.addNoArgInstruction(ILOAD_0);
 		output.addNoArgInstruction(ILOAD_1);
-		output.addBranchInstruction(IFLE, "updateB");
-		// Update a
-		output.addNoArgInstruction(ILOAD_0); // a
-		output.addNoArgInstruction(ILOAD_1); // b
-		output.addNoArgInstruction(ISUB); // a - b
-		output.addNoArgInstruction(ISTORE_0); // a = a - b
-		output.addBranchInstruction(GOTO, "endIf");
-		output.addLabel("updateB");
-		// Update b
-		output.addNoArgInstruction(ILOAD_1); // b
-		output.addNoArgInstruction(ILOAD_0); // a
-		output.addNoArgInstruction(ISUB); // b - a
-		output.addNoArgInstruction(ISTORE_1); // b = b - a
-		output.addLabel("endIf");
-		// Branching instruction
-		output.addLabel("out");
+		output.addBranchInstruction(IF_ICMPLE, "updateB");
+		output.addNoArgInstruction(ILOAD_0);
 		output.addNoArgInstruction(ILOAD_1);
-		output.addNoArgInstruction(ICONST_0);
-		output.addBranchInstruction(IFNE, "statement");
-		// Return
-		output.addNoArgInstruction(ILOAD_0); // Maybe error because we reload the initial value ? And should use the value a
+		output.addNoArgInstruction(ISUB);
+		output.addNoArgInstruction(ISTORE_0);
+		output.addBranchInstruction(GOTO, "in");
+		
+		output.addLabel("updateB");
+		output.addNoArgInstruction(ILOAD_1);
+		output.addNoArgInstruction(ILOAD_0);
+		output.addNoArgInstruction(ISUB);
+		output.addNoArgInstruction(ISTORE_1);
+		output.addBranchInstruction(GOTO, "in");
+		
+		output.addLabel("out");
+		output.addNoArgInstruction(ILOAD_0);
 		output.addNoArgInstruction(IRETURN);
+		
 		
 		// Write
 		output.write();
