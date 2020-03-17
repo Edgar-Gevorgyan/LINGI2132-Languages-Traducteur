884444be4c9170ac98600ab0546b83319b13d7d4
louisna <navarre.louis@student.uclouvain.be>
2020-03-17 19:11:03 +0100

    add theory 3

--- Assignment2/j--/src/lingi2132/Generator.java
+++ Assignment2/j--/src/lingi2132/Generator.java
@@ -4,23 +4,6 @@ import java.util.ArrayList;
 import static jminusminus.CLConstants.*;
 import jminusminus.CLEmitter;
 
-/**
- *package packageOfClassToGenerate;
- *
- *public class ClassToGenerate {
- *	public static int gcd(int a, int b) {
- *		while (b != 0) {
- *			if (a > b) {
- *				a = a - b;
- *			} else {
- *				b = b - a;
- *			}
- *		}
- *	return a;
- *	}
- *}
- */
-
 
 public class Generator extends GlobalGenerator {
 	public Generator(String s) {
