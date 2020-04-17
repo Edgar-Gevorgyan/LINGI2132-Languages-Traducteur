fe50915cfc2a93813e45d9ee28a23528e3435086
egevorgyan <edgar.gevorgyan@student.uclouvain.be>
2020-04-17 20:49:25 +0200

    tests de base ok

--- Assignment3/lt-project-master/shared/src/main/scala/DSLDemo/CanvasElementModifier.scala
+++ Assignment3/lt-project-master/shared/src/main/scala/DSLDemo/CanvasElementModifier.scala
@@ -5,5 +5,26 @@ trait CanvasElementModifier[ApplyOn <: Shape] {
 }
 
 case class Color(c: String) extends CanvasElementModifier[Shape] {
-  override def change(x: Shape): Unit = ???
+  override def change(x: Shape): Unit = x.colorShape(c)
+}
+
+case class Width(w: Int) extends CanvasElementModifier[Shape] {
+  override def change(x: Shape): Unit = x match {
+    case Rectangle(_,_, _, _) => x.asInstanceOf[Rectangle].width = w
+    case _ => throw new IllegalArgumentException("no width attribute")
+  }
+}
+
+case class Height(h: Int) extends CanvasElementModifier[Shape] {
+  override def change(x: Shape): Unit = x match {
+    case Rectangle(_,_,_,_) => x.asInstanceOf[Rectangle].height = h
+    case _ => throw new IllegalArgumentException("no height attribute")
+  }
+}
+
+case class Radius(r: Int) extends CanvasElementModifier[Shape] {
+  override def change(x: Shape): Unit = x match {
+    case Circle(_,_,_) => x.asInstanceOf[Circle].radius = r
+    case _ => throw new IllegalArgumentException("no radius attribute")
+  }
 }
--- Assignment3/lt-project-master/shared/src/main/scala/DSLDemo/Shapes.scala
+++ Assignment3/lt-project-master/shared/src/main/scala/DSLDemo/Shapes.scala
@@ -1,26 +1,47 @@
 package DSLDemo
+import ComposedShapeImplicits._
+
 
 sealed trait Shape {
-  def and(s: Shape) : ComposedShape = ???
-  def change(property: CanvasElementModifier[Shape]): Unit = ???
-  def moveX(v: Int): Unit = ???
-
-  def moveY(v: Int): Unit = ???
+  def and(s: Shape) : ComposedShape = new ComposedShape(List(this, s))
+  def ++(s: Shape) : ComposedShape = this.and(s)
+  def change(property: CanvasElementModifier[Shape]): Unit = property.change(this)
+  def moveX(v: Int): Unit
+  def moveY(v: Int): Unit
+  def colorShape(c: String) : Unit
 }
 
 sealed trait ShapeAttributes {
   var color: String = "red"
   var strokeWidth: Int = 1
   // Add more attributes here
+  var posX: Int = 0
+  var posY: Int = 0
+  def color(c:String):Unit = color = c
 }
 
-case class ComposedShape() extends Shape {
-  def map(f: Shape => Shape) : ComposedShape = ???
-  def flatMap(f: Shape => IterableOnce[Shape]) : ComposedShape = ???
-  def foreach[B](f: Shape => B) : Unit = ???
-  def apply(i: Int): Shape = ???
+object ComposedShapeImplicits {
+  implicit def List2ComposedShape(shapes: List[Shape]):ComposedShape = new ComposedShape(shapes)
 }
 
-case class Rectangle(var x: Int, var y: Int, var width: Int, var height: Int) extends Shape with ShapeAttributes
+case class ComposedShape(shapes: List[Shape]) extends Shape {
+  def map(f: Shape => Shape) : ComposedShape = new ComposedShape(this.shapes.map(f))
+  def flatMap(f: Shape => IterableOnce[Shape]) : ComposedShape = new ComposedShape(this.shapes.flatMap(f))
+  def foreach[B](f: Shape => B) : Unit = shapes.foreach(f)
+  def apply(i: Int): Shape = shapes(i)
+  override def moveX(v: Int): Unit = this.foreach(s => s.moveX(v))
+  override def moveY(v: Int): Unit = this.foreach(s => s.moveY(v))
+  override def colorShape(c: String): Unit = this.foreach(s => s.colorShape(c))
+}
 
-case class Circle(var x: Int, var y: Int, var radius: Int) extends Shape with ShapeAttributes
+case class Rectangle(var x: Int, var y: Int, var width: Int, var height: Int) extends ShapeAttributes with Shape {
+  override def moveX(v: Int): Unit = x += v
+  override def moveY(v: Int): Unit = y += v
+  override def colorShape(c: String): Unit = color = c
+}
+
+case class Circle(var x: Int, var y: Int, var radius: Int) extends Shape with ShapeAttributes {
+  override def moveX(v: Int): Unit = x += v
+  override def moveY(v: Int): Unit = y += v
+  override def colorShape(c: String): Unit = color = c
+}
