34718db9e9535cbb468969eba618d1a14eec2a40
louisna <louisnavarre@hotmail.com>
2020-04-22 23:55:49 +0200

    remove space

--- Assignment3/lt-project-master/shared/src/main/scala/DSLDemo/Shapes.scala
+++ Assignment3/lt-project-master/shared/src/main/scala/DSLDemo/Shapes.scala
@@ -2,14 +2,15 @@ package DSLDemo
 
 
 sealed trait Shape {
-  def and(s: Shape) : ComposedShape = ComposedShape(List(this, s))
-  def +(s: Shape) : ComposedShape = this.and(s)
+  def and(s: Shape): ComposedShape = ComposedShape(List(this, s))
+  def +(s: Shape): ComposedShape = this.and(s)
   def change(property: CanvasElementModifier[Shape]): Unit
   def moveX(v: Int): Unit
   def moveY(v: Int): Unit
-  def color(c: String) : Unit
-  def strokeWidth(sW : Int) : Unit
+  def color(c: String): Unit
+  def strokeWidth(sW : Int): Unit
 }
+
 object Shape {
   implicit def ArrayRectangle2ComposedShape(shapes:Array[Rectangle]): ComposedShape = ComposedShape(shapes.toList)
   implicit def ArrayCircle2ComposedShape(shapes:Array[Circle]): ComposedShape = ComposedShape(shapes.toList)
@@ -32,10 +33,10 @@ sealed trait SingleShape extends Shape with ShapeAttributes {
 }
 
 case class ComposedShape(shapes: List[Shape]) extends Shape {
-  def ++(cs: ComposedShape) : ComposedShape = ComposedShape(this.shapes ++ cs.shapes)
-  def map(f: Shape => Shape) : ComposedShape = ComposedShape(this.shapes.map(f))
-  def flatMap(f: Shape => IterableOnce[Shape]) : ComposedShape = ComposedShape(this.shapes.flatMap(f))
-  def foreach[B](f: Shape => B) : Unit = shapes.foreach(f)
+  def ++(cs: ComposedShape): ComposedShape = ComposedShape(this.shapes ++ cs.shapes)
+  def map(f: Shape => Shape): ComposedShape = ComposedShape(this.shapes.map(f))
+  def flatMap(f: Shape => IterableOnce[Shape]): ComposedShape = ComposedShape(this.shapes.flatMap(f))
+  def foreach[B](f: Shape => B): Unit = shapes.foreach(f)
   def apply(i: Int): Shape = shapes(i)
   override def moveX(v: Int): Unit = this.foreach(s => s.moveX(v))
   override def moveY(v: Int): Unit = this.foreach(s => s.moveY(v))
@@ -44,6 +45,8 @@ case class ComposedShape(shapes: List[Shape]) extends Shape {
   override def strokeWidth(sW: Int): Unit = this.foreach(s => s.strokeWidth(sW))
 }
 
-case class Rectangle(var x: Int, var y: Int, var width: Int, var height: Int) extends SingleShape
+case class Rectangle(var x: Int, var y: Int, var width: Int, var height: Int) extends SingleShape {
+  def change(property: CanvasElementModifier[Rectangle]): Unit = property.change(this)
+}
 
 case class Circle(var x: Int, var y: Int, var radius: Int) extends SingleShape
