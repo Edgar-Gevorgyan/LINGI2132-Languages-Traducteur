42035efb378042e338013cd8a1aa8e23d7a0e649
egevorgyan <edgar.gevorgyan@student.uclouvain.be>
2020-04-26 11:52:13 +0200

    add mode key code

--- Assignment3/lt-project-master/js/src/main/scala/DSL/Utils/Observable.scala
+++ Assignment3/lt-project-master/js/src/main/scala/DSL/Utils/Observable.scala
@@ -4,6 +4,32 @@ import org.scalajs.dom
 import org.scalajs.dom.document
 
 object Key{
+  val A:    Int = 65
+  val B:    Int = 66
+  val C:    Int = 67
+  val D:    Int = 68
+  val E:    Int = 69
+  val F:    Int = 70
+  val G:    Int = 71
+  val H:    Int = 72
+  val I:    Int = 73
+  val J:    Int = 74
+  val K:    Int = 75
+  val L:    Int = 76
+  val M:    Int = 77
+  val N:    Int = 78
+  val O:    Int = 79
+  val P:    Int = 80
+  val Q:    Int = 81
+  val R:    Int = 82
+  val S:    Int = 83
+  val T:    Int = 84
+  val U:    Int = 85
+  val V:    Int = 86
+  val W:    Int = 87
+  val X:    Int = 88
+  val Y:    Int = 89
+  val Z:    Int = 90
   val LEF:  Int = 37
   val UP:   Int = 38
   val RIGHT:Int = 39
